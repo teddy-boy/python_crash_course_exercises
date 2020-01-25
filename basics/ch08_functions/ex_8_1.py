860f359452ff515e8777958e128e0b9f2d09f861
Dzung Nguyen <dzungnguyen.learntocode@gmail.com>
2020-01-25 17:32:44 +0700

    add some examples and exercises in chap 8

---
+++ basics/ch08_functions/ex_8_1.py
@@ -0,0 +1,5 @@
+def display_message():
+    print("I learn how to define a function.")
+
+
+display_message()
---
+++ basics/ch08_functions/ex_8_2.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    print(f"One of my favorite book is {title.title()}.")
+
+
+favorite_book('reminiscence of a stock operator')
