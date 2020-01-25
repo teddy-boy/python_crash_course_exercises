08ec5037aa4de72261b83f85b30f90cba6a329de
Dzung Nguyen <dzungnguyen.learntocode@gmail.com>
2020-01-25 19:49:57 +0700

    add examples for 'Passing Arguments' section of chapter 8

---
+++ basics/ch08_functions/pets.py
@@ -0,0 +1,11 @@
+# Positional Arguments
+
+
+def describe_pet(animal_type, pet_name):
+    # Display information about a pet
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet('dog', 'min')
+describe_pet('cat', 'miu')
---
+++ basics/ch08_functions/pets_1.py
@@ -0,0 +1,10 @@
+# Keyword Arguments
+
+
+def describe_pet(animal_type, pet_name):
+    # Display information about a pet
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet(animal_type='dog', pet_name='min')
