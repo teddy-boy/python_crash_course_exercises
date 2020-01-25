fef1b474f9dc090e708fcfb5ea9bfd716ec1116b
Dzung Nguyen <dzungnguyen.learntocode@gmail.com>
2020-01-26 00:05:33 +0700

    add ex_8_4.py and ex_8_5.py

---
+++ basics/ch08_functions/ex_8_4.py
@@ -0,0 +1,7 @@
+def make_shirt(size='large', text='i love python'):
+	print(f"You order a {size} shirt with the text '{text.title()}' on it.")
+
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt(text='i also love c sharp')
---
+++ basics/ch08_functions/ex_8_5.py
@@ -0,0 +1,7 @@
+def describe_city(city='hanoi', country='vietnam'):
+	print(f"{city.title()} is in {country.title()}.")
+
+
+describe_city()
+describe_city('ho chi minh')
+describe_city('paris', 'france')
