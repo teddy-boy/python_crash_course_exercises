0fd9051e9e33e287234a9adb5ae2b7dd74fdae48
Dzung Nguyen <dzungnguyen.learntocode@gmail.com>
2020-01-25 21:36:07 +0700

    delete all .vscode folder, add ex_8_3.py

---
+++ basics/ch08_functions/ex_8_3.py
@@ -0,0 +1,8 @@
+def make_shirt(size, text):
+    print("Summaries of the order:")
+    print(f"Size: {size}")
+    print(f"Text: {text}")
+
+
+make_shirt('large', 'Labor is glory')
+make_shirt(text="I don't care", size='medium')
