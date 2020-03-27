bb3e6db9f6f0c7db9b8cc87933ae3c2016e1dfe6
Dzung Nguyen <dzungnguyen.learntocode@gmail.com>
2020-03-27 22:34:25 +0700

    Add dog.py in chapter 09 - classes

---
+++ basics/ch09_classes/dog.py
@@ -0,0 +1,29 @@
+class Dog():
+    """A simple attempt to model a dog."""
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name.title()} is now sitting.")
+    
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name.title()} rolled over!")
+
+my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
+
+print(f"My dog's name is {my_dog.name.title()}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+my_dog.roll_over()
+
+print("-----------------------------------")
+
+print(f"Your dog's name is {your_dog.name.title()}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
+your_dog.roll_over()
